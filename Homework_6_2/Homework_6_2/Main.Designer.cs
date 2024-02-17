403c7672dca291001218a2c598a0ab7693c83935
git-adam <awwojcik1@gmail.com>
2024-02-17 10:57:01 +0100

    Base functionality

--- Homework_6_2/Homework_6_2/Main.Designer.cs
+++ Homework_6_2/Homework_6_2/Main.Designer.cs
@@ -1,7 +1,9 @@
 ﻿
+using System;
+
 namespace Homework_6_2
 {
-    partial class Form1
+    partial class Main
     {
         /// <summary>
         /// Required designer variable.
@@ -29,13 +31,93 @@ namespace Homework_6_2
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.pbAddedPhoto = new System.Windows.Forms.PictureBox();
+            this.ofdBrowsePhoto = new System.Windows.Forms.OpenFileDialog();
+            this.btnAddPhoto = new System.Windows.Forms.Button();
+            this.tbPhotoPath = new System.Windows.Forms.TextBox();
+            this.btnDeletePhoto = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pbAddedPhoto)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pbAddedPhoto
+            // 
+            this.pbAddedPhoto.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.pbAddedPhoto.BackColor = System.Drawing.SystemColors.GrayText;
+            this.pbAddedPhoto.Location = new System.Drawing.Point(12, 12);
+            this.pbAddedPhoto.Name = "pbAddedPhoto";
+            this.pbAddedPhoto.Size = new System.Drawing.Size(776, 374);
+            this.pbAddedPhoto.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pbAddedPhoto.TabIndex = 0;
+            this.pbAddedPhoto.TabStop = false;
+            // 
+            // ofdBrowsePhoto
+            // 
+            this.ofdBrowsePhoto.Title = "Przeglądaj zdjęcia";
+            // 
+            // btnAddPhoto
+            // 
+            this.btnAddPhoto.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnAddPhoto.BackColor = System.Drawing.Color.DarkOliveGreen;
+            this.btnAddPhoto.Location = new System.Drawing.Point(654, 404);
+            this.btnAddPhoto.Name = "btnAddPhoto";
+            this.btnAddPhoto.Size = new System.Drawing.Size(109, 45);
+            this.btnAddPhoto.TabIndex = 1;
+            this.btnAddPhoto.Text = "Dodaj";
+            this.btnAddPhoto.UseVisualStyleBackColor = false;
+            this.btnAddPhoto.Click += new System.EventHandler(this.btnAddPhoto_Click);
+            // 
+            // tbPhotoPath
+            // 
+            this.tbPhotoPath.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tbPhotoPath.Location = new System.Drawing.Point(12, 415);
+            this.tbPhotoPath.Name = "tbPhotoPath";
+            this.tbPhotoPath.ReadOnly = true;
+            this.tbPhotoPath.Size = new System.Drawing.Size(620, 20);
+            this.tbPhotoPath.TabIndex = 2;
+            // 
+            // btnDeletePhoto
+            // 
+            this.btnDeletePhoto.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnDeletePhoto.BackColor = System.Drawing.Color.Brown;
+            this.btnDeletePhoto.Location = new System.Drawing.Point(654, 448);
+            this.btnDeletePhoto.Name = "btnDeletePhoto";
+            this.btnDeletePhoto.Size = new System.Drawing.Size(109, 48);
+            this.btnDeletePhoto.TabIndex = 3;
+            this.btnDeletePhoto.Text = "Usuń";
+            this.btnDeletePhoto.UseVisualStyleBackColor = false;
+            this.btnDeletePhoto.Visible = false;
+            this.btnDeletePhoto.Click += new System.EventHandler(this.btnDeletePhoto_Click);
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.ClientSize = new System.Drawing.Size(805, 508);
+            this.Controls.Add(this.btnDeletePhoto);
+            this.Controls.Add(this.tbPhotoPath);
+            this.Controls.Add(this.btnAddPhoto);
+            this.Controls.Add(this.pbAddedPhoto);
+            this.Name = "Main";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Zdjęcie";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Main_FormClosed);
+            ((System.ComponentModel.ISupportInitialize)(this.pbAddedPhoto)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.PictureBox pbAddedPhoto;
+        private System.Windows.Forms.OpenFileDialog ofdBrowsePhoto;
+        private System.Windows.Forms.Button btnAddPhoto;
+        private System.Windows.Forms.TextBox tbPhotoPath;
+        private System.Windows.Forms.Button btnDeletePhoto;
     }
 }
 
--- Homework_6_2/Homework_6_2/Main.cs
+++ Homework_6_2/Homework_6_2/Main.cs
@@ -1,20 +1,96 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Homework_6_2.Properties;
+using System;
+using System.IO;
 using System.Windows.Forms;
 
 namespace Homework_6_2
 {
-    public partial class Form1 : Form
+    public partial class Main : Form
     {
-        public Form1()
+        private string _filePath = "";
+
+        public string PhotoPath 
+        {
+            get
+            {
+                return Settings.Default.PhotoPath;
+            }
+            set
+            {
+                Settings.Default.PhotoPath = value;
+            }
+        }
+
+        public Main()
         {
             InitializeComponent();
+
+            InitImageLocationOnStart();
         }
+
+
+        private void InitImageLocationOnStart()
+        {
+            if (Settings.Default.PhotoPath == "")
+            {
+                var confirmDismiss = MessageBox.Show($"Czy chcesz wczytać zdjęcie domyślne?", "Wczytanie zdjęcia domyślnego", MessageBoxButtons.OKCancel);
+
+                if (confirmDismiss == DialogResult.OK)
+                {
+                    int lowerLimit = 1, upperLimit = 14;
+                    var rnd = new Random();
+                    var drawnNumber = rnd.Next(lowerLimit, upperLimit);
+                    SetImageLocation(Path.Combine(Path.GetDirectoryName(Application.ExecutablePath), $@"resource\{drawnNumber}.jpg"));
+                }
+            }
+            else
+            {
+                if (!File.Exists(Settings.Default.PhotoPath))
+                    MessageBox.Show("Plik o podanej ścieżce nie istnieje!", "Komunikat", MessageBoxButtons.OK);
+                SetImageLocation(Settings.Default.PhotoPath);
+            }
+        }
+
+        private void SetImageLocation(string path)
+        {
+            _filePath = path;
+            tbPhotoPath.Text = _filePath;
+            pbAddedPhoto.ImageLocation = _filePath;
+            btnDeletePhoto.Visible = true;
+        }
+
+        private void btnAddPhoto_Click(object sender, EventArgs e)
+
+        {
+            ofdBrowsePhoto.FilterIndex = 1;
+            ofdBrowsePhoto.Filter = "Picture|*.jpg;*.jpeg;*.png;|All files (*.*)|*.*";
+
+            if (_filePath == "")
+                ofdBrowsePhoto.InitialDirectory = Environment.CurrentDirectory;
+            else
+                ofdBrowsePhoto.InitialDirectory = _filePath;
+
+            ofdBrowsePhoto.ShowDialog();
+
+            SetImageLocation(ofdBrowsePhoto.FileName.ToString());
+            
+        }
+
+        private void btnDeletePhoto_Click(object sender, EventArgs e)
+        {
+            SetImageLocation("");
+            btnDeletePhoto.Visible = false;
+        }
+
+        private void Main_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            if (_filePath == null)
+                Settings.Default.PhotoPath = "";
+            else
+                Settings.Default.PhotoPath = _filePath;
+
+            Settings.Default.Save();
+        }
+
     }
 }
